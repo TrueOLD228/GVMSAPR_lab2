35702aa250e6791db70dcc2655311acefe2a8f21
pavel poryvaev <vovka_v.e@mail.ru>
2022-01-27 20:11:02 +0700

    LAB2

---
+++ Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace GVMSAPR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.buttonIncrease = new System.Windows.Forms.Button();
+            this.buttonDecrease = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.ReflectX = new System.Windows.Forms.Button();
+            this.buttonRotateRight = new System.Windows.Forms.Button();
+            this.buttonRotateLeft = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.buttonInvert = new System.Windows.Forms.Button();
+            this.buttonContrastTest = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonSharp = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(791, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(599, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Текущее разрешение изображения";
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Location = new System.Drawing.Point(606, 58);
+            this.labelX.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(23, 13);
+            this.labelX.TabIndex = 6;
+            this.labelX.Text = "X =";
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Cursor = System.Windows.Forms.Cursors.Default;
+            this.labelY.Location = new System.Drawing.Point(693, 58);
+            this.labelY.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(23, 13);
+            this.labelY.TabIndex = 7;
+            this.labelY.Text = "Y =";
+            // 
+            // buttonIncrease
+            // 
+            this.buttonIncrease.BackgroundImage = global::GVMSAPR.Properties.Resources.Increase;
+            this.buttonIncrease.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonIncrease.Location = new System.Drawing.Point(536, 158);
+            this.buttonIncrease.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonIncrease.Name = "buttonIncrease";
+            this.buttonIncrease.Size = new System.Drawing.Size(54, 57);
+            this.buttonIncrease.TabIndex = 9;
+            this.buttonIncrease.UseVisualStyleBackColor = true;
+            this.buttonIncrease.Click += new System.EventHandler(this.buttonIncrease_Click);
+            // 
+            // buttonDecrease
+            // 
+            this.buttonDecrease.BackgroundImage = global::GVMSAPR.Properties.Resources.Decrease_;
+            this.buttonDecrease.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDecrease.Location = new System.Drawing.Point(478, 158);
+            this.buttonDecrease.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonDecrease.Name = "buttonDecrease";
+            this.buttonDecrease.Size = new System.Drawing.Size(54, 57);
+            this.buttonDecrease.TabIndex = 8;
+            this.buttonDecrease.UseVisualStyleBackColor = true;
+            this.buttonDecrease.Click += new System.EventHandler(this.buttonDecrease_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::GVMSAPR.Properties.Resources.ReflectY;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button4.Location = new System.Drawing.Point(536, 95);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(54, 57);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // ReflectX
+            // 
+            this.ReflectX.BackgroundImage = global::GVMSAPR.Properties.Resources.icons8_отразить_по_вертикали_96;
+            this.ReflectX.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ReflectX.Location = new System.Drawing.Point(478, 95);
+            this.ReflectX.Margin = new System.Windows.Forms.Padding(2);
+            this.ReflectX.Name = "ReflectX";
+            this.ReflectX.Size = new System.Drawing.Size(54, 57);
+            this.ReflectX.TabIndex = 3;
+            this.ReflectX.UseVisualStyleBackColor = true;
+            this.ReflectX.Click += new System.EventHandler(this.ReflectX_Click);
+            // 
+            // buttonRotateRight
+            // 
+            this.buttonRotateRight.BackgroundImage = global::GVMSAPR.Properties.Resources.RotateRight;
+            this.buttonRotateRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonRotateRight.Location = new System.Drawing.Point(536, 32);
+            this.buttonRotateRight.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonRotateRight.Name = "buttonRotateRight";
+            this.buttonRotateRight.Size = new System.Drawing.Size(54, 57);
+            this.buttonRotateRight.TabIndex = 2;
+            this.buttonRotateRight.UseVisualStyleBackColor = true;
+            this.buttonRotateRight.Click += new System.EventHandler(this.buttonRotateRight_Click);
+            // 
+            // buttonRotateLeft
+            // 
+            this.buttonRotateLeft.BackgroundImage = global::GVMSAPR.Properties.Resources.RotateLeft;
+            this.buttonRotateLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonRotateLeft.Location = new System.Drawing.Point(478, 32);
+            this.buttonRotateLeft.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonRotateLeft.Name = "buttonRotateLeft";
+            this.buttonRotateLeft.Size = new System.Drawing.Size(54, 57);
+            this.buttonRotateLeft.TabIndex = 1;
+            this.buttonRotateLeft.UseVisualStyleBackColor = true;
+            this.buttonRotateLeft.Click += new System.EventHandler(this.buttonRotateLeft_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(5, 32);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.MaximumSize = new System.Drawing.Size(429, 399);
+            this.pictureBox1.MinimumSize = new System.Drawing.Size(429, 399);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(429, 399);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonInvert
+            // 
+            this.buttonInvert.Location = new System.Drawing.Point(478, 239);
+            this.buttonInvert.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonInvert.Name = "buttonInvert";
+            this.buttonInvert.Size = new System.Drawing.Size(92, 19);
+            this.buttonInvert.TabIndex = 10;
+            this.buttonInvert.Text = "Инверсия";
+            this.buttonInvert.UseVisualStyleBackColor = true;
+            this.buttonInvert.Click += new System.EventHandler(this.buttonInvert_Click);
+            // 
+            // buttonContrastTest
+            // 
+            this.buttonContrastTest.Location = new System.Drawing.Point(4, 54);
+            this.buttonContrastTest.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonContrastTest.Name = "buttonContrastTest";
+            this.buttonContrastTest.Size = new System.Drawing.Size(113, 19);
+            this.buttonContrastTest.TabIndex = 11;
+            this.buttonContrastTest.Text = "Подтвердить";
+            this.buttonContrastTest.UseVisualStyleBackColor = true;
+            this.buttonContrastTest.Click += new System.EventHandler(this.buttonContrast_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.buttonContrastTest);
+            this.groupBox1.Location = new System.Drawing.Point(602, 239);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(122, 78);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Контрастность";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 15);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "от -100 до 100";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 31);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // buttonSharp
+            // 
+            this.buttonSharp.Location = new System.Drawing.Point(478, 291);
+            this.buttonSharp.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonSharp.Name = "buttonSharp";
+            this.buttonSharp.Size = new System.Drawing.Size(92, 20);
+            this.buttonSharp.TabIndex = 13;
+            this.buttonSharp.Text = "Четкость";
+            this.buttonSharp.UseVisualStyleBackColor = true;
+            this.buttonSharp.Click += new System.EventHandler(this.Sharpen_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 440);
+            this.Controls.Add(this.buttonSharp);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonInvert);
+            this.Controls.Add(this.buttonIncrease);
+            this.Controls.Add(this.buttonDecrease);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.ReflectX);
+            this.Controls.Add(this.buttonRotateRight);
+            this.Controls.Add(this.buttonRotateLeft);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonRotateLeft;
+        private System.Windows.Forms.Button buttonRotateRight;
+        private System.Windows.Forms.Button ReflectX;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.Button buttonDecrease;
+        private System.Windows.Forms.Button buttonIncrease;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button buttonInvert;
+        private System.Windows.Forms.Button buttonContrastTest;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonSharp;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GVMSAPR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private Bitmap bitmap;
+        private Bitmap contrastBitmap;
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Файлы изображений| *.bmp; *.png;* .jpg; |Все файлы|*.*";
+            if (openFileDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            try
+            {
+                bitmap = (Bitmap)Bitmap.FromFile(openFileDialog1.FileName);
+                saveFileDialog1.FileName = openFileDialog1.FileName;
+                pictureBox1.Image = bitmap;
+                labelY.Text = "Y=" + bitmap.Size.Width;
+                labelX.Text = "X=" + bitmap.Size.Height;
+
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+                return;
+            }
+        }
+
+        private void buttonRotateLeft_Click(object sender, EventArgs e)
+        {
+            if (bitmap != null)
+            {
+                bitmap.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                pictureBox1.Image = bitmap;
+            }
+
+
+        }
+
+        private void buttonRotateRight_Click(object sender, EventArgs e)
+        {
+            bitmap.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            pictureBox1.Image = bitmap;
+        }
+
+        private void ReflectX_Click(object sender, EventArgs e)
+        {
+            bitmap.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            pictureBox1.Image = bitmap;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bitmap.RotateFlip(RotateFlipType.RotateNoneFlipY);
+            pictureBox1.Image = bitmap;
+        }
+
+        private void buttonDecrease_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bitmap = new Bitmap(bitmap, bitmap.Size.Width / 3 * 2, bitmap.Size.Height / 3 * 2);
+                pictureBox1.Image = bitmap;
+                labelY.Text = "Y=" + bitmap.Size.Width;
+                labelX.Text = "X=" + bitmap.Size.Height;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void buttonIncrease_Click(object sender, EventArgs e)
+        {
+            if (bitmap.Size.Width < 5000 && bitmap.Size.Height < 5000)
+            {
+                try
+                {
+                    bitmap = new Bitmap(bitmap, bitmap.Size.Width * 3 / 2, bitmap.Size.Height * 3 / 2);
+                    pictureBox1.Image = bitmap;
+                    labelY.Text = "Y=" + bitmap.Size.Width;
+                    labelX.Text = "X=" + bitmap.Size.Height;
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var bitmapSave = bitmap;
+                bitmapSave.Save(saveFileDialog1.FileName);
+            }
+            catch (Exception NullReferenceException)
+            {
+
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Файлы изображений| *.bmp; *.png;* .jpg; |Все файлы|*.*";
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            bitmap.Save(saveFileDialog1.FileName);
+        }
+
+        private void buttonInvert_Click(object sender, EventArgs e)
+        {
+            for (int Y = 0; Y < bitmap.Height; Y++)
+            {
+                for (int X = 0; X < bitmap.Width; X++)
+                {
+                    Color oldColor = bitmap.GetPixel(X, Y);
+                    Color newColor = Color.FromArgb(oldColor.A, 255 - oldColor.R, 255 - oldColor.G, 255 - oldColor.B);
+                    bitmap.SetPixel(X, Y, newColor);
+                    pictureBox1.Image = bitmap;
+                }
+            }
+        }
+
+        private void buttonContrast_Click(object sender, EventArgs e)
+        {
+            float contrastTemp = 0;
+            try
+            {
+                contrastTemp = Convert.ToInt16(textBox1.Text);
+            }
+            catch (Exception)
+            {
+
+            }
+            if (contrastTemp > 100 || contrastTemp < -100)
+            {
+                return;
+            }
+            float contrast = 1 + (contrastTemp / 200);
+            for (int Y = 0; Y < bitmap.Height; Y++)
+            {
+                for (int X = 0; X < bitmap.Width; X++)
+                {
+                    Color oldColor = bitmap.GetPixel(X, Y);
+                    byte newRed;
+                    byte newGreen;
+                    byte newBlue;
+                    if (oldColor.R * contrast > 255)
+                    {
+                        newRed = Convert.ToByte(255);
+                    }
+                    else if (oldColor.R * contrast < 0)
+                    {
+                        newRed = Convert.ToByte(0);
+                    }
+                    else
+                    {
+                        newRed = Convert.ToByte(oldColor.R * contrast);
+                    }
+
+                    if (oldColor.G * contrast > 255)
+                    {
+                        newGreen = Convert.ToByte(255);
+                    }
+                    else if (oldColor.G * contrast < 0)
+                    {
+                        newGreen = Convert.ToByte(0);
+                    }
+                    else
+                    {
+                        newGreen = Convert.ToByte(oldColor.G * contrast);
+                    }
+
+                    if (oldColor.B * contrast > 255)
+                    {
+                        newBlue = Convert.ToByte(255);
+                    }
+                    else if (oldColor.B * contrast < 0)
+                    {
+                        newBlue = Convert.ToByte(0);
+                    }
+                    else
+                    {
+                        newBlue = Convert.ToByte(oldColor.B * contrast);
+                    }
+
+                    Color newColor = Color.FromArgb(oldColor.A, newRed, newGreen, newBlue);
+                    bitmap.SetPixel(X, Y, newColor);
+                }
+            }
+
+            pictureBox1.Image = bitmap;
+        }
+
+        public static Bitmap sharpen(Bitmap image, int power)
+        {
+            Bitmap sharpenImage = new Bitmap(image.Width, image.Height);
+            int filterWidth = power;
+            int filterHeight = power;
+            int w = image.Width;
+            int h = image.Height;
+            double[,] filter = new double[filterWidth, filterHeight];
+            filter[0, 0] = filter[0, 1] = filter[0, 2] = filter[1, 0] = filter[1, 2] = filter[2, 0] = filter[2, 1] = filter[2, 2] = -1; filter[1, 1] = 9;
+            double factor = 1.0;
+            double bias = 0.0;
+            Color[,] result = new Color[image.Width, image.Height];
+            for (int x = 0; x < w; ++x)
+            {
+                for (int y = 0; y < h; ++y)
+                {
+                    double red = 0.0, green = 0.0, blue = 0.0;
+                    for (int filterX = 0; filterX < filterWidth; filterX++)
+                    {
+                        for (int filterY = 0; filterY < filterHeight; filterY++)
+                        {
+                            int imageX = (x - filterWidth / 2 + filterX + w) % w;
+                            int imageY = (y - filterHeight / 2 + filterY + h) % h;
+                            Color imageColor = image.GetPixel(imageX, imageY);
+                            red += imageColor.R * filter[filterX, filterY];
+                            green += imageColor.G * filter[filterX, filterY];
+                            blue += imageColor.B * filter[filterX, filterY];
+                        }
+                        int r = Math.Min(Math.Max((int)(factor * red + bias), 0), 255);
+                        int g = Math.Min(Math.Max((int)(factor * green + bias), 0), 255);
+                        int b = Math.Min(Math.Max((int)(factor * blue + bias), 0), 255);
+                        result[x, y] = Color.FromArgb(r, g, b);
+                    }
+                }
+            }
+            for (int i = 0; i < w; ++i)
+            {
+                for (int j = 0; j < h; ++j)
+                {
+                    sharpenImage.SetPixel(i, j, result[i, j]);
+                }
+            }
+            return sharpenImage;
+        }
+
+        private void Sharpen_click(object sender, EventArgs e)
+        {
+           bitmap = sharpen(bitmap,5);
+           pictureBox1.Image = bitmap;
+        }
+    }
+}
